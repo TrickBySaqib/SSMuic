e4b9e95cb729393f8db97bfba9eec6f393d3f757
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2024-09-26 12:46:53 +0530

    Update Youtube.py

--- PBXMUSIC/platforms/Youtube.py
+++ PBXMUSIC/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from BABYMUSIC.utils.database import is_on_off
-from BABYMUSIC.utils.formatters import time_to_seconds
+from PBXMUSIC.utils.database import is_on_off
+from PBXMUSIC.utils.formatters import time_to_seconds
 
 
 
